b5c7e81ec10eea040a04081b1e26f351a4f794fe
John Mo <peikaimo@gmail.com>
2025-01-30 15:19:13 -0600

    fixed workflow.yml and working on book collection 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Book.create!([
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", price: 10, published_date: "1925-04-10" },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", price: 12, published_date: "1960-07-11" },
+  { title: "1984", author: "George Orwell", price: 9, published_date: "1949-06-08" },
+  { title: "Pride and Prejudice", author: "Jane Austen", price: 8, published_date: "1813-01-28" },
+  { title: "The Hobbit", author: "J.R.R. Tolkien", price: 14, published_date: "1937-09-21" }
+])
