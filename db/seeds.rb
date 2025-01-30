874a03ac6ab657f7d8ec9025c19a4e7b903ccc44
John Mo <peikaimo@gmail.com>
2025-01-30 15:45:16 -0600

    testing seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,8 @@ Book.create!([
   { title: "To Kill a Mockingbird", author: "Harper Lee", price: 12, published_date: "1960-07-11" },
   { title: "1984", author: "George Orwell", price: 9, published_date: "1949-06-08" },
   { title: "Pride and Prejudice", author: "Jane Austen", price: 8, published_date: "1813-01-28" },
-  { title: "The Hobbit", author: "J.R.R. Tolkien", price: 14, published_date: "1937-09-21" }
+  { title: "The Hobbit", author: "J.R.R. Tolkien", price: 14, published_date: "1937-09-21" },
+  { title: "The Catcher in the Rye", author: "J.D. Salinger", price: 11, published_date: "1951-07-16" },
+  { title: "The Lord of the Rings", author: "J.R.R. Tolkien", price: 20, published_date: "1954-07-29" },
+  { title: "Animal Farm", author: "George Orwell", price: 7, published_date: "1945-08-17" },
 ])
