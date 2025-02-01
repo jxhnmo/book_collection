1c572790d75de847dc830772b4b48328c3ee1b96
John Mo <peikaimo@gmail.com>
2025-01-31 21:59:23 -0600

    seeding fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,51 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Book.create!([
-  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", price: 10, published_date: "1925-04-10" },
-  { title: "To Kill a Mockingbird", author: "Harper Lee", price: 12, published_date: "1960-07-11" },
-  { title: "1984", author: "George Orwell", price: 9, published_date: "1949-06-08" },
-  { title: "Pride and Prejudice", author: "Jane Austen", price: 8, published_date: "1813-01-28" },
-  { title: "The Hobbit", author: "J.R.R. Tolkien", price: 14, published_date: "1937-09-21" },
-  { title: "The Catcher in the Rye", author: "J.D. Salinger", price: 11, published_date: "1951-07-16" },
-  { title: "The Lord of the Rings", author: "J.R.R. Tolkien", price: 20, published_date: "1954-07-29" },
-  { title: "Animal Farm", author: "George Orwell", price: 7, published_date: "1945-08-17" },
-])
+Book.find_or_create_by!(title: "The Great Gatsby") do |book|
+    book.author = "F. Scott Fitzgerald"
+    book.price = 10
+    book.published_date = "1925-04-10"
+  end
+  
+  Book.find_or_create_by!(title: "To Kill a Mockingbird") do |book|
+    book.author = "Harper Lee"
+    book.price = 12
+    book.published_date = "1960-07-11"
+  end
+  
+  Book.find_or_create_by!(title: "1984") do |book|
+    book.author = "George Orwell"
+    book.price = 9
+    book.published_date = "1949-06-08"
+  end
+  
+  Book.find_or_create_by!(title: "Pride and Prejudice") do |book|
+    book.author = "Jane Austen"
+    book.price = 8
+    book.published_date = "1813-01-28"
+  end
+  
+#   Book.find_or_create_by!(title: "The Hobbit") do |book|
+#     book.author = "J.R.R. Tolkien"
+#     book.price = 14
+#     book.published_date = "1937-09-21"
+#   end
+  
+#   Book.find_or_create_by!(title: "The Catcher in the Rye") do |book|
+#     book.author = "J.D. Salinger"
+#     book.price = 11
+#     book.published_date = "1951-07-16"
+#   end
+  
+#   Book.find_or_create_by!(title: "The Lord of the Rings") do |book|
+#     book.author = "J.R.R. Tolkien"
+#     book.price = 20
+#     book.published_date = "1954-07-29"
+#   end
+  
+#   Book.find_or_create_by!(title: "Animal Farm") do |book|
+#     book.author = "George Orwell"
+#     book.price = 7
+#     book.published_date = "1945-08-17"
+#   end
+  
